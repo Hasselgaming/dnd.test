274a5f49b027a8a481439d2169954c69cb6d3736
Mikael Lehrmann Kashlev <mika3033@edu.aarhustech.dk>
2022-03-23 13:36:24 +0100

    Made some changes

---
+++ src/Board.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Square from "./Square";
+import Knight from "./Knight";
+
+export default function Board(){
+    return(
+        <div>
+            <Square black>
+                <Knight/>
+            </Square>
+
+        </div>
+
+    )
+
+}
