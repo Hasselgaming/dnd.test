af03ce033b10e9f5767110de856a5fbbb43bf617
Mikael Lehrmann Kashlev <mika3033@edu.aarhustech.dk>
2022-03-23 14:01:17 +0100

    Made some more changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import './App.css';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Knight from './Knight'
+import Square from './Square'
+import Board from './Board'
 
-ReactDOM.render(<Knight />, document.getElementById('root'))
+ReactDOM.render(
+  <Square black>
+    <Knight />
+  </Square>,
+  document.getElementById('root')
+)
+
+ReactDOM.render(
+  <Board knightPosition={[0, 0]} />,
+  document.getElementById('root')
+)
 
 function App() {
   return (
--- src/Square.js
+++ src/Square.js
@@ -16,5 +16,5 @@ export default function Square({ black, children }) {
         {children}
       </div>
     )
-  }
 
+  }
